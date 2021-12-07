de7b7166b93a08ce0b8718288be7e06dc5e86ad3
rafiqulislam21 <ribijoy19@gmail.com>
2021-12-07 11:31:43 +0100

    1153 and 1154 presentation issue fix

--- URI_1153_Simple_Factorial.py
+++ URI_1153_Simple_Factorial.py
@@ -4,4 +4,4 @@ N = int(input())
 if 0 < N < 13:
     for i in range(1, N+1):
         fact_sum *= i        
-    print("{} ".format(fact_sum))
+    print("{}".format(fact_sum))
--- URI_1154_Ages.py
+++ URI_1154_Ages.py
@@ -1,12 +1,15 @@
 age = 0
 age_sum = 0
 age_counter = 0
-while(age >= 0):
+while(True):
     age = int(input())
-    age_sum += age
-    age_counter += 1
-    
+    if age < 0:
+        break
+    else:
+        age_sum += age
+        age_counter += 1
+        
 age_average = age_sum/age_counter
 
-print(round(age_average,3))
+print("{:.2f}".format(age_average))
     
