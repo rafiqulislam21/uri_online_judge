20f22854f486f93572ee0d2e1ed6d113f8f745a2
rafiqulislam21 <ribijoy19@gmail.com>
2021-12-23 23:14:00 +0100

    1185

---
+++ URI_1185_Above_the_secundary_diagonal.py
@@ -0,0 +1,19 @@
+arr_size = 12
+T = input()
+M=[]
+
+for i in range(arr_size):
+    for j in range(arr_size):
+        x = float(input())
+        if i < arr_size and  j < arr_size-i-1:
+            M.append(x)
+        
+sum = 0
+for i in M:
+    sum = sum + i
+        
+if T == 'S':
+    print("{:.1f}".format(sum))
+elif T == 'M':
+    median= sum/len(M)
+    print("{:.1f}".format(median))
